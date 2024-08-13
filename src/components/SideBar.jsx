d54a1dd196d1a97cae6c816e132594ae4975d42a
Richard <olugbenga.rich@gmail.com>
2024-08-13 08:21:01 +0100

    Built SearchBar, Updated Feed, Building SideBar.

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+const SideBar = () => {
+  return <div>SideBar</div>;
+};
+
+export default SideBar;
